2f79d5961f645a648dc204d5cb2308c85df720d2
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-02 17:38:51 +0600

    dsajskdgfhdsakolgdbsaifgobfa

---
+++ activity2.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter The 1st Number: "))
+
+num2 = float(input("Enter The 2nd Number: "))
+
+if (num1 % num2) == 0:
+    print (f"{num1} is divisble by {num2}")
+
+else:
+    print (f"{num1} is not divisble by {num2}")
