35a9a0596ef1796bec9303291deb7351f026b8a6
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-02 17:56:33 +0600

    s,kjifoiuhjopskt5uhle

---
+++ activity3.py
@@ -0,0 +1,13 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+
+sum = total_number * mean1
+print (sum)
+
+correct = sum - wrong_number + correct_number
+print (correct)
+
+result = correct / total_number
+print (result)
