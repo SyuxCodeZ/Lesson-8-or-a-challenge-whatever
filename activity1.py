44b9b59870c2989936303f8b8b2edd089ae9ab47
SyuxCodeZ <cuberdytm@gmail.com>
2025-01-02 17:32:13 +0600

    jhsdfgdfjsdfjsdfjsnewkejhfgd

---
+++ activity1.py
@@ -0,0 +1,19 @@
+a = 5
+b = 78
+c = 2
+d = 342
+e = 3
+
+result = (d / b) * a + e - c
+
+print (result)
+
+name = "Abdullah"
+
+age = "12"
+
+if name == "Abdullah" or name == "Abidullah" and age >= 10:
+    print ("Hello")
+
+else:
+    print ("Bye Bye")
